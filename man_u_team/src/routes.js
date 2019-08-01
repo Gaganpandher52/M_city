be606452eb66c232d226c05997f621a146e0587e
gaganpandher52 <gaganpandher52@gmail.com>
2019-07-31 22:24:13 -0600

    added main layout comp and header comp

--- man_u_team/src/routes.js
+++ man_u_team/src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Layout from './Hoc/Layout'
 
-function App() {
-  return (
-    <div className="App">
-      hello
-      
-    </div>
-  );
+const Routes = (props) =>{
+  return(
+    <Layout>
+      second line
+    </Layout>
+  )
 }
 
-export default App;
+export default Routes;
