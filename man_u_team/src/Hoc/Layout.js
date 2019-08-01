2a2ecdb2cc94bc6a1cde364a86ce15aa14329667
gaganpandher52 <gaganpandher52@gmail.com>
2019-07-31 22:30:26 -0600

    built header using material ui and added styling

--- man_u_team/src/Hoc/Layout.js
+++ man_u_team/src/Hoc/Layout.js
@@ -4,7 +4,7 @@ import Header from '../components/Header_footer/Header'
 const Layout = (props) => {
   return (
     <div>
-      <Header/ >
+      <Header/>
       {props.children}
     </div>
   );
--- man_u_team/src/components/Header_footer/Header.js
+++ man_u_team/src/components/Header_footer/Header.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <AppBar
+        position='fixed'
+        style={{
+          backgroundColor:'#98c5e9',
+          boxShadow:'none',
+          padding:'10px 0',
+          borderBottom: '2px solid #00285e'
+
+        }}
+      
+      >
+        <ToolBar
+          style={{display:'flex'}}
+        
+        ></ToolBar>
+
+      </AppBar>
     );
   }
 }
