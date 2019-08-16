491f6a819a1eedd05606b71d7cfa16bfb5d7895d
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-16 14:26:06 -0600

    added new home route

--- man_u_team/src/routes.js
+++ man_u_team/src/routes.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Layout from './Hoc/Layout'
 import { Switch, Route } from 'react-router-dom'
 
+import Home from './components/home'
+
 const Routes = (props) =>{
   return(
     <Layout>
       <Switch>
-        
+        <Route exact component={Home} path='/' />
+
       </Switch>
       
     </Layout>
