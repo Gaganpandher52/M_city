783db2d7c7cc2dd4638cb1cf6107a01349fac455
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-16 14:58:55 -0600

    adding state and complex animation

--- man_u_team/src/components/home/featured/Stripes.js
+++ man_u_team/src/components/home/featured/Stripes.js
@@ -4,16 +4,71 @@ import Animate from 'react-move/Animate'
 
 class Stripes extends Component {
 
+  state = {
+    stripes:[
+      {
+        background:'#98c5e9',
+        left: 120,
+        rotate: 24,
+        top: -260,
+        delay:0
+      },
+      {
+        background:'#ffffff',
+        left: 360,
+        rotate: 25,
+        top: -397,
+        delay: 200
+      },
+      {
+        background:'#98c5e9',
+        left: 600,
+        rotate: 25,
+        top: -498,
+        delay: 400
+      }
+    ]
+  }
+
   showStripes = () =>(
-    <div>
-      stripes 
-    </div>
+    this.state.stripes.map((stripe,i) =>(
+      <Animate
+        key={i}
+        show={true}
+
+        start={{
+          background:'#ffffff',
+          opacity:0,
+          left:0
+        }}
+
+        enter={{
+          background:[stripe.background],
+          opacity:[1],
+          left:[stripe.left], 
+          timing:{delay:500, duration:200, ease:easePolyOut}
+        }}
+      >
+        {({opacity,left,background})=>{
+          return(
+            <div
+              className='stripe'
+              style={{
+                background,
+                opacity,
+                transform:`rotate(0deg) translate(${left}0px,0px)`
+              }}
+            ></div>
+          );
+        }}
+      </Animate>
+    ))
   )
 
 
   render() {
     return (
-      <div className='feartured_stripes'>
+      <div className='featured_stripes'>
         {this.showStripes()}
         
       </div>
