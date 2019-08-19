c8d8e4adee5631d26afdb3caf0c1841c2c793119
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-19 13:50:05 -0600

    added new text comp

---
+++ man_u_team/src/components/home/featured/Text.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { easePolyOut } from 'd3-ease'
+import Animate from 'react-move/Animate'
+
+const Text = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Text;
