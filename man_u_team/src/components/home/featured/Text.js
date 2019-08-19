538edd50e1716edf53d577381e0ae758bf20ef74
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-19 14:02:04 -0600

    changed text comp from rsc to rcc

--- man_u_team/src/components/home/featured/Text.js
+++ man_u_team/src/components/home/featured/Text.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { easePolyOut } from 'd3-ease'
 import Animate from 'react-move/Animate'
 
-const Text = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+class Text extends Component {
+  animateNumber = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity:0,
+        rotate:0
+      }}
+      enter={{
+        opacity:[1],
+        rotate:[360],
+        timing:{duartion: 1000, ease:easePolyOut}
+      }}
+    >
+      {({opacity,rotate})=>{
+        return (
+          <div className='featured_number'
+            style={{
+              opacity,
+              trasform:`translate(260px,170px) rotateY(${rotate}deg)`
+            }}
+          >
+
+          </div>
+        )
+
+      }}
+    </Animate>
+  )
+
+  render() {
+    return (
+      <div className='featured_text'>
+        {this.animateNumber()}
+        
+      </div>
+    );
+  }
+}
 
 export default Text;
