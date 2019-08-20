752c6a48ef09241501fbe16f7b7a4b02c4f3d8fb
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-20 13:08:18 -0600

    added new aminateFirst

--- man_u_team/src/components/home/featured/Text.js
+++ man_u_team/src/components/home/featured/Text.js
@@ -32,10 +32,41 @@ class Text extends Component {
     </Animate>
   )
 
+  animateFirst = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity:0,
+        rotate:0
+      }}
+      enter={{
+        opacity:[1],
+        rotate:[360],
+        timing:{duartion: 1000, ease:easePolyOut}
+      }}
+    >
+      {({opacity,rotate})=>{
+        return (
+          <div className='featured_number'
+            style={{
+              opacity,
+              transform:`translate(260px,170px) rotateY(${rotate}deg)`
+            }}
+          >
+          3
+          </div>
+        )
+
+      }}
+    </Animate>
+
+  )
+
   render() {
     return (
       <div className='featured_text'>
         {this.animateNumber()}
+        {this.animateFirst()}
         
       </div>
     );
