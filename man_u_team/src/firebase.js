1804e7763a8de1cec783fefb1decf1cf84605739
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-22 12:41:21 -0600

    firebase connection working

--- man_u_team/src/firebase.js
+++ man_u_team/src/firebase.js
@@ -13,3 +13,12 @@ const firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig)
+
+const firebaseDB = firebase.database()
+
+firebaseDB.ref('matches').once('value').then((snapshot)=>{
+  console.log(snapshot.val())
+
+
+   
+})
