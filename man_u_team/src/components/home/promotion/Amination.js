e583510ccb8fd1ad95f5a7e13682e7da3e56ceef
gaganpandher52 <gaganpandher52@gmail.com>
2019-08-27 15:20:28 -0600

    added new promotion comp

---
+++ man_u_team/src/components/home/promotion/Amination.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PromotionAnimation = () => {
+  return (
+    <div className>
+
+      animation
+      
+    </div>
+  );
+};
+
+export default PromotionAnimation;
