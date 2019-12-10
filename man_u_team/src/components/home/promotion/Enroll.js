36ef462b613fcb4836dab65fbb9cd6324d80ac79
gaganpandher52 <gaganpandher52@gmail.com>
2019-12-10 14:01:05 -0700

    added logic to submit email

--- man_u_team/src/components/home/promotion/Enroll.js
+++ man_u_team/src/components/home/promotion/Enroll.js
@@ -50,7 +50,7 @@
     })
   }
 
-  resetFormSuccess(){
+  resetFormSuccess(type){
     const newFormdata = {...this.state.formdata};
 
     for(let key in newFormdata){
@@ -62,7 +62,7 @@
     this.setState({
       formError:false,
       formdata:newFormdata,
-      formSuccess:'Congratulations'
+      formSuccess:type ? 'Congratulations' : "Already on the database"
     });
     this.successMessage();
     
@@ -92,7 +92,14 @@
      if(formIsValid){
       firebasePromotions.orderByChild('email').equalTo(dataToSubmit.email).once('value')
       .then((snapshot)=>{
-        console.log(snapshot.val());
+        if(snapshot.val() === null){
+          firebasePromotions.push(dataToSubmit);
+          this.resetFormSuccess(true)
+        }else{
+          this.resetFormSuccess(false)
+
+        }
+        
 
       })
       
