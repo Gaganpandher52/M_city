2cace04dddc182554b078aae830aa1cb060fdcfb
gaganpandher52 <gaganpandher52@gmail.com>
2020-02-27 19:42:57 -0700

    removing fade wrapper

--- man_u_team/src/components/home/promotion/Enroll.js
+++ man_u_team/src/components/home/promotion/Enroll.js
@@ -113,33 +113,33 @@
    
    render() {
      return (
-       <Fade>
-        <div className='enroll_wrapper'>
-          <form onSubmit={(event)=>this.submitForm(event)}>
-            <div className='enroll_title'>
-              Enter your email
-            </div>
-            <div className='enroll_input'>
-              <FormField
-                id={'email'}
-                formdata={this.state.formdata.email}
-                change={(element)=>this.updateForm(element)}
+      //  <Fade>
+      //   <div className='enroll_wrapper'>
+      //     <form onSubmit={(event)=>this.submitForm(event)}>
+      //       <div className='enroll_title'>
+      //         Enter your email
+      //       </div>
+      //       <div className='enroll_input'>
+      //         <FormField
+      //           id={'email'}
+      //           formdata={this.state.formdata.email}
+      //           change={(element)=>this.updateForm(element)}
                 
-              />
-              {this.state.formError ? 
-                <div className='error_label'>Something is wrong, try again</div>
-                :null
-              }
-              <div className='success_label'>{this.state.formSuccess}</div>
-              <button onClick={(event)=>this.submitForm(event)}>Enroll</button>
-              <div className='enroll_discl'>Only one email per person</div>
+      //         />
+      //         {this.state.formError ? 
+      //           <div className='error_label'>Something is wrong, try again</div>
+      //           :null
+      //         }
+      //         <div className='success_label'>{this.state.formSuccess}</div>
+      //         <button onClick={(event)=>this.submitForm(event)}>Enroll</button>
+      //         <div className='enroll_discl'>Only one email per person</div>
               
-            </div>
+      //       </div>
 
-          </form>
+      //     </form>
           
-        </div>
-       </Fade>
+      //   </div>
+      //  </Fade>
      );
    }
  }
